e4a1b9a9e2f23cb2af4b651b8bcfa161327096f7
sga-mudu <411221402@gms.ndhu.edu.tw>
2025-07-12 16:32:52 +0800

    change

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Api from "./API/Api.jsx"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MovieDetail from "./supports/movieDetail.jsx";
+
+
+function App() {
+  return(<>
+    <Routes>
+        <Route path="/" element={<Api/>}/>
+        <Route path="/movie/:id" element={<MovieDetail/>}/>
+        <Route path="*" element={<h1>Page Not Found</h1>}/>
+    </Routes>
+  </>);
+}
+
+export default App
