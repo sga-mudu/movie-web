8e93ab9663c6bf934665f8420d6bf306092076e3
sga-mudu <411221402@gms.ndhu.edu.tw>
2025-07-17 17:46:49 +0800

    hhhhh

--- src/supports/movieDetail.jsx
+++ src/supports/movieDetail.jsx
@@ -1,41 +1,54 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import Axios from 'axios';
-// import './MovieDetail.css';
+
+import './MovieDetail.css'; // Keep only this import
 
 function MovieDetail() {
-  const { id } = useParams();
-  const [movie, setMovie] = useState(null);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    Axios.get(`https://www.omdbapi.com/?apikey=15d90a23&i=${id}`)
-      .then((response) => {
-        setMovie(response.data);
-      });
-  }, [id]);
-
-  if (!movie){
-    return <div className="loading-spinner">
-                <div className="spinner"></div>
-            </div>;
-  }
-    
-
-  return (
-    <>
-        <div className="container">
-            <button onClick={() => navigate((-1))}>
-                Go back
-            </button>
-
-            <div className="second-container">
-                <h1>{movie.Title}</h1>
-                <p>{movie.Plot}</p>
-                <img src={movie.Poster} alt={movie.Title} />
-            </div>
-        </div>
-    </>
-  );
+  return <div>Test</div>;
 }
 export default MovieDetail;
+
+
+
+
+
+
+// import { useNavigate, useParams } from 'react-router-dom';
+// import { useState, useEffect } from 'react';
+// import Axios from 'axios';
+// import './MovieDetail.css';
+
+// function MovieDetail() {
+//   const { id } = useParams();
+//   const [movie, setMovie] = useState(null);
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     Axios.get(`https://www.omdbapi.com/?apikey=15d90a23&i=${id}`)
+//       .then((response) => {
+//         setMovie(response.data);
+//       });
+//   }, [id]);
+
+//   if (!movie){
+//     return <div className="loading-spinner">
+//                 <div className="spinner"></div>
+//             </div>;
+//   }
+    
+
+//   return (
+//     <>
+//         <div className="container">
+//             <button onClick={() => navigate((-1))}>
+//                 Go back
+//             </button>
+
+//             <div className="second-container">
+//                 <h1>{movie.Title}</h1>
+//                 <p>{movie.Plot}</p>
+//                 <img src={movie.Poster} alt={movie.Title} />
+//             </div>
+//         </div>
+//     </>
+//   );
+// }
+// export default MovieDetail;
