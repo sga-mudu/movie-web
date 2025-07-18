7aab454fc4674d7a438446c046f4cc6d5d06a853
sga-mudu <411221402@gms.ndhu.edu.tw>
2025-07-18 14:39:21 +0800

    ccchange

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   css: {
     modules: false,  // Disable if you’re not using CSS Modules
   },
