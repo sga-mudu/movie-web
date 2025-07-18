d518b0b3f410cd8d06e8627efb946e78ebbbbb58
sga-mudu <411221402@gms.ndhu.edu.tw>
2025-07-18 14:13:42 +0800

    change

--- src/supports/movieDetail.jsx
+++ src/supports/movieDetail.jsx
@@ -1,54 +1,42 @@
 
-import './MovieDetail.css'; // Keep only this import
+import { useNavigate, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import './movieDetail.css';
 
 function MovieDetail() {
-  return <div>Test</div>;
-}
-export default MovieDetail;
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    Axios.get(`https://www.omdbapi.com/?apikey=15690a23&i=${id}`)
+      .then((response) => {
+        setMovie(response.data);
+      });
+  }, [id]);
 
-
-
-
-
-// import { useNavigate, useParams } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
-// import Axios from 'axios';
-// import './MovieDetail.css';
-
-// function MovieDetail() {
-//   const { id } = useParams();
-//   const [movie, setMovie] = useState(null);
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     Axios.get(`https://www.omdbapi.com/?apikey=15d90a23&i=${id}`)
-//       .then((response) => {
-//         setMovie(response.data);
-//       });
-//   }, [id]);
-
-//   if (!movie){
-//     return <div className="loading-spinner">
-//                 <div className="spinner"></div>
-//             </div>;
-//   }
+  if (!movie){
+    return <div className="loading-spinner">
+                <div className="spinner"></div>
+            </div>;
+  }
     
 
-//   return (
-//     <>
-//         <div className="container">
-//             <button onClick={() => navigate((-1))}>
-//                 Go back
-//             </button>
+  return (
+    <>
+        <div className="container">
+            <button onClick={() => navigate((-1))}>
+                Go back
+            </button>
 
-//             <div className="second-container">
-//                 <h1>{movie.Title}</h1>
-//                 <p>{movie.Plot}</p>
-//                 <img src={movie.Poster} alt={movie.Title} />
-//             </div>
-//         </div>
-//     </>
-//   );
-// }
-// export default MovieDetail;
+            <div className="second-container">
+                <h1>{movie.Title}</h1>
+                <p>{movie.Plot}</p>
+                <img src={movie.Poster} alt={movie.Title} />
+            </div>
+        </div>
+    </>
+  );
+}
+export default MovieDetail;
